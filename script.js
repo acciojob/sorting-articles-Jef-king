d2845c4c1075a7177273366f4d085f3913cc8165
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-03-29 19:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-let bandlist=document.querySelector("band");
+
+let bandList=document.querySelector("#band");
+
 function bandSort(a, b) {
     const ignoreWord = ['a', 'an', 'the'];
     const getTitle = (band) => band.replace(/^(the |an |a )/i, '');
@@ -9,9 +11,40 @@ function bandSort(a, b) {
 
     return titleA.localeCompare(titleB);
 }
-bands.sort(bandSort());
+
+// bands.sort(bandSort);
+
+// bands.forEach((element) => {
+//     let listItem = document.createElement('li');
+//     listItem.textContent = element;
+//     bandList.appendChild(listItem);
+
+//     // Create and append horizontal line
+//     let horizontal = document.createElement('hr');
+// 	horizontal.classList ="lineHr"
+//     bandlist.appendChild(horizontal);
+// });
+let unorderedList = document.querySelector("#band");
+
+function customSort(a, b) {
+    const ignoreWord = ['a', 'an', 'the'];
+    const getTitle = (band) => band.replace(/^(the |an |a )/i, '');
+    const titleA = getTitle(a);
+    const titleB = getTitle(b);
+
+    return titleA.localeCompare(titleB);
+}
+
+// bands.sort(customSort);
+// bands.sort(customSort);
+bands.sort(bandSort);
 bands.forEach((element) => {
-    let listBand = document.createElement('li');
-    listBand.textContent = element;
-    unorderedList.appendChild(listBand);
+    let listItem = document.createElement('li');
+    listItem.textContent = element;
+    bandList.appendChild(listItem);
+
+    // Create and append horizontal line
+    let horizontal = document.createElement('hr');
+	horizontal.classList ="lineHr"
+    unorderedList.appendChild(horizontal);
 });
